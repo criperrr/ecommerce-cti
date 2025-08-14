135fa6051092f2d03cf71f0a21dc55ad3e1a4de8
criperrr <gabriel.camaforte@unesp.br>
2025-08-13 21:21:32 -0300

    i dont remember. my laptop bios just bricked lol

--- scripts/script.js
+++ scripts/script.js
@@ -2,60 +2,61 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aboutButton = document.querySelector('#about-a a');
     const aboutDialog = document.querySelector('dialog.about');
-    const settingsButton = document.getElementById('settings-a');
-    const settingsDialog = document.getElementById('settings-dialog');
-    const settingsForm = document.getElementById('settings-form');
-    const closeButton = document.querySelector('#settings-dialog .close-button');
+    const accountButton = document.getElementById('account-a');
+    const accountDialog = document.getElementById('account-dialog');
+    const accountForm = document.getElementById('account-form');
+    const closeButton = document.querySelector('#account-dialog .close-button');
     const themeSelect = document.getElementById('toggle');
-
-    function applyTheme(theme) {
+    
+    
+    function applyTheme(theme){
         document.body.setAttribute('data-theme', theme);
         themeSelect.value = theme;
-    }
-
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    applyTheme(savedTheme);
+        themeSelect.checked = (theme === 'dark');
+        localStorage.setItem('theme', theme);
+    } 
+    (() => applyTheme(localStorage.getItem('theme') || 'light'))();
 
     aboutButton.addEventListener('click', function (event) {
         event.preventDefault();
         aboutDialog.showModal();
     });
 
-    settingsButton.addEventListener('click', function (event) {
+    accountButton.addEventListener('click', function (event) {
         event.preventDefault();
         event.stopPropagation(); // O javascript passa o evento (click) para os elementos pais, coloquei um listener no document, ent qnd ele chegar no document ele aciona e fecha o dialog. pra evitar isso, tem essa função que para a propagação do evento.
-        const isOpen = settingsDialog.classList.toggle('open'); // metodo q retorna true se a classe foi adicionada e false se foi removida
-        settingsButton.setAttribute('aria-expanded', isOpen); // faz o aria-expanded refletir o estado atual do dialog, ai ele fica expandido ou n
+        const isOpen = accountDialog.classList.toggle('open'); // metodo q retorna true se a classe foi adicionada e false se foi removida
+        accountButton.setAttribute('aria-expanded', isOpen); // faz o aria-expanded refletir o estado atual do dialog, ai ele fica expandido ou n
     });
 
     closeButton.addEventListener('click', () => {
-        settingsDialog.classList.remove('open');
-        settingsButton.setAttribute('aria-expanded', 'false');
+        accountDialog.classList.remove('open');
+        accountButton.setAttribute('aria-expanded', 'false');
     });
 
     themeSelect.addEventListener('change', function (event) {
         event.preventDefault();
         const selectedTheme = themeSelect.checked ? 'dark' : 'light';
         applyTheme(selectedTheme);
-        localStorage.setItem('theme', selectedTheme);
+        
     })
 
-    settingsForm.addEventListener('submit', function (event) {
+    accountForm.addEventListener('submit', function (event) {
         event.preventDefault();
         // submit logic here if needed
 
-        settingsDialog.classList.remove('open');
-        settingsButton.setAttribute('aria-expanded', 'false');
+        accountDialog.classList.remove('open');
+        accountButton.setAttribute('aria-expanded', 'false');
     });
 
     document.addEventListener('click', () => {
-        if (settingsDialog.classList.contains('open')) {
-            settingsDialog.classList.remove('open');
-            settingsButton.setAttribute('aria-expanded', 'false');
+        if (accountDialog.classList.contains('open')) {
+            accountDialog.classList.remove('open');
+            accountButton.setAttribute('aria-expanded', 'false');
         }
     });
 
-    settingsDialog.addEventListener('click', (event) => {
+    accountDialog.addEventListener('click', (event) => {
         event.stopPropagation();
     });
 });
