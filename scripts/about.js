a227e0862e673858d930358903a2fe608475f83c
heit00 <heitorutyama@gmail.com>
2025-08-23 17:48:31 -0300

    feat/se houver erros usar o commit imediatamente anterior

---
+++ scripts/about.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dialog = document.getElementById('about-page-dialog');
+    const button = document.getElementById('close-dialog-button');
+
+    if(!dialog || !button){
+        console.log('<ERRO> ELEMENTOS NÃO ENCONTRADOS!');
+        return;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+});
