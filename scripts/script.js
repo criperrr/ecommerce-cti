f38106b3dfe98db4b058d8c759a1b4ffb7393866
criperrr <gabriel.camaforte@unesp.br>
2025-08-23 17:05:43 -0300

    improved organization (or not)

--- scripts/script.js
+++ scripts/script.js
@@ -1,80 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const accountButton = document.getElementById('account-a');
-    const accountDialog = document.getElementById('account-dialog');
-    const accountForm = document.getElementById('account-form');
-    const closeButton = document.querySelector('#account-dialog .close-button');
+    const accountButton = document.getElementById('account-a')?? document.getElementById('settings-a');
+    const accountDialog = document.getElementById('account-dialog')?? document.getElementById('settings-dialog');
+    const accountForm = document.getElementById('account-form')?? document.getElementById('settings-form');
+    const closeButton = document.querySelector('#account-dialog .close-button')?? document.querySelector('#settings-dialog .close-button');
     const themeSelect = document.getElementById('toggle');
-    const textInputs = document.querySelectorAll('.custom-input');
-
-
-
-    textInputs.forEach((input) => {
-        const label = input.previousElementSibling;
-        const occultText = label.dataset.occultText;
-        const originalText = label.textContent || '';
-        let currentIntervals = [];
-
-        if (input.value.trim() !== '') {
-            label.classList.add('filled')
-        } else {
-            label.classList.remove('filled')
-        } // verifica assim que o DOM carrega e faz o forEach.
-
-        input.addEventListener('input', () => {
-            if (input.value.trim() !== '') {
-                label.classList.add('filled')
-            } else {
-                label.classList.remove('filled')
-            }
-        });
-
-        function clearIntervals() {
-            currentIntervals.forEach(clearInterval);
-            currentIntervals = [];
-        };
-
-        const animateText = (label, newText) => {
-            clearIntervals(); // para todos os intervalos dentro do array
-            let currentText = label.textContent;
-            if (!occultText || input.value.trim() !== '') return;
-            
-            const eraseInterval = setInterval(() => {
-                if (currentText.length > 0) {
-                    console.log("oi");
-                    currentText = currentText.slice(0, -1);
-                    label.textContent = currentText;
-                } else {
-                    clearInterval(eraseInterval); // se for 0 ent ele para o interval anterior e cria o typeinterval
-                    let i = 0;
-                    const typeInterval = setInterval(() => {
-                        if (i < newText.length) {
-                            label.textContent += newText[i];
-                            i++;
-                        } else {
-                            clearInterval(typeInterval);
-                        }
-                        
-                    }, 10);  //velocidade pra escrever
-                    currentIntervals.push(typeInterval); // colocar o typeinteval e o erase interval na array pra evitar q os dois rodem simultaneamente
-                }
-               
-            }, 10); //velocidade pra apgar 
-            currentIntervals.push(eraseInterval);
-        };
-
-        input.addEventListener('focus', () => {
-            if (occultText) {
-                animateText(label, occultText);
-            }
-        });
-
-        input.addEventListener('blur', () => {
-            if (input.value.trim() === '') {
-                animateText(label, originalText);
-            }
-        });
-    })
     
     
 
@@ -94,9 +24,11 @@ document.addEventListener('DOMContentLoaded', function () {
         accountButton.setAttribute('aria-expanded', isOpen); // faz o aria-expanded refletir o estado atual do dialog, ai ele fica expandido ou n
     });
 
+
     closeButton.addEventListener('click', () => {
-        accountDialog.classList.remove('open');
+         accountDialog.classList.remove('open');
         accountButton.setAttribute('aria-expanded', 'false');
+
     });
 
     themeSelect.addEventListener('change', function (event) {
@@ -124,4 +56,6 @@ document.addEventListener('DOMContentLoaded', function () {
     accountDialog.addEventListener('click', (event) => {
         event.stopPropagation();
     });
+
+    
 });
